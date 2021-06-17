4dfdc34a9ed6c7419a5960733ca766b1c36cb21a
huangyan <1621740748@qq.com>
2021-06-17 18:22:55 +0800

    提交

--- src/main/java/com/atguigu/boot/controller/HelloController.java
+++ src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.atguigu.boot.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    @Value("${cms.name}")
+	private String cmsName;
     @RequestMapping("/hello")
     public String handle01() {
-        return "Hello, Spring Boot 2!";
+        return "Hello, Spring Boot 2!"+cmsName;
     }
 }
